4aea037ee329a17232b4b310ab970d9d917f8cea
Tobias Rickmann <tobias.rickmann@pp-cloud.local>
2018-01-11 13:59:26 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import { View } from './core/View';
+import { Router } from './core/Router';
+import { Controller } from './core/Controller';
+
+window.onload = () => {
+    console.log("App is running");
+};
